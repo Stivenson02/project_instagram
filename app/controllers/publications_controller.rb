f358479c70991cbcb7854a16444c7cc8efc4d7f0
Stivenson02 <stivenson02@gmail.com>
2022-03-22 12:41:18 -0500

    adjust devise

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,5 @@
 class PublicationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_publication, only: %i[ show edit update destroy ]
 
   # GET /publications or /publications.json
